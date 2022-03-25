479f84d6f8f0dec2055029fd491fc1c77d1935f7
robledo-dev <robledo.nicolas.edu@gmail.com>
2022-03-25 20:32:38 -0300

    archivo Js de unidad dos

---
+++ unidad-dos.js
@@ -0,0 +1,23 @@
+/*Declarar una variable de nombre cantidadDeVentanasEnMiCasa y asignarle la cantidad de ventanas que hay en tu casa. Mostrar el valor de la variable por la consola.
+Declarar una variable de nombre soyHumano y asignarle el valor true (verdadero). Mostrar el valor de la variable por la consola.
+Declarar una variable de nombre miGustoDePizzaPreferido y asignarle la variedad de pizza que más te guste usando un String (cadena de caracteres). Mostrar el valor de la variable por la consola.
+Mostrar por la consola los siguientes valores false, 'Hola', 0 y 'Minions' usando la misma variable.*/
+
+var cantidadDeVentanasEnMiCasa=3;
+console.log("Cantidad de ventanas en mi casa"+" "+cantidadDeVentanasEnMiCasa);
+
+var soyHumano=true;
+console.log("Soy humano "+soyHumano);
+
+var miGustoDePizzaPreferido="Fugazzeta";
+console.log("Mi gusto de pizza preferido es"+" "+miGustoDePizzaPreferido);
+
+var comodin="Hola";
+console.log("La misma variable con valor reasignado"+" "+comodin);
+
+var comodin=0;
+console.log("La misma variable con valor reasignado"+" "+comodin);
+
+var comodin="Minions";
+console.log("La misma variable con valor reasignado"+" "+comodin);
+
